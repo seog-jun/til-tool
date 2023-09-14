4ca1bd53768bc73d0f5be3d689e3072dc5cd0d2d
seog-jun <shong69@myseneca.ca>
2023-09-14 08:10:25 -0400

    Added stylesheet option

--- src/index.ts
+++ src/index.ts
@@ -11,20 +11,24 @@ const path = require("path");
 const fs = require("fs");
 
 // Command line inter face to use options
-program.name("ts-node src/index.ts").usage("-i <fileName> / <dirName>");
+program
+  .name("ts-node src/index.ts")
+  .usage("-i <fileName> -o output <dirname> -s <stylesheetURL>");
 
 program.version(
   myInfo.name + " " + myInfo.version,
   "-v, --version",
-  "output the current version"
+  "outputs the current version"
 );
-program.option("-i, --input <filename>", "converts file to html");
-program.option("-o, --output <filename>", "creates a specified directory");
+program.option("-i, --input <fileName>", "converts file to html");
+program.option("-o, --output <dirName>", "creates a specified directory");
+program.option("-s, --stylesheet <stylesheetURL>", "sets a stylesheet to HTML");
 const options = program.opts();
 program.parse(process.argv);
 
 const inputValue = program.opts().input;
 const outputValue = program.opts().output;
+const styleValue = program.opts().stylesheet;
 
 if (options.input) {
   fs.rmSync(join(__dirname, `../til`), { recursive: true, force: true });
@@ -33,26 +37,53 @@ if (options.input) {
   // a single .txt file is used as an input
   if (inputValue.includes(".txt")) {
     const filename = path.parse(inputValue).name;
-    writeFileSync(
-      join(__dirname, `../til/${filename}.html`),
-      writeFile.htmlCreator(
-        readFile.syncReadFile(join("../../", inputValue)),
-        filename
-      )
-    );
+
+    if (options.stylesheet) {
+      writeFileSync(
+        join(__dirname, `../til/${filename}.html`),
+        writeFile.htmlCreator(
+          readFile.syncReadFile(join("../../", inputValue)),
+          filename,
+          styleValue
+        )
+      );
+    } else {
+      writeFileSync(
+        join(__dirname, `../til/${filename}.html`),
+        writeFile.htmlCreator(
+          readFile.syncReadFile(join("../../", inputValue)),
+          filename
+        )
+      );
+    }
   }
   // a directory is used as an input
   else {
     const filesArray = readDir.readDirectory(join("../../", inputValue));
-    for (let i = 0; i < filesArray.length; i++) {
-      const filename = path.parse(filesArray[i]).name;
-      writeFileSync(
-        join(__dirname, `../til/${filename}.html`),
-        writeFile.htmlCreator(
-          readFile.syncReadFile(join("../../", inputValue, filesArray[i])),
-          filename
-        )
-      );
+
+    if (options.stylesheet) {
+      for (let i = 0; i < filesArray.length; i++) {
+        const filename = path.parse(filesArray[i]).name;
+        writeFileSync(
+          join(__dirname, `../til/${filename}.html`),
+          writeFile.htmlCreator(
+            readFile.syncReadFile(join("../../", inputValue, filesArray[i])),
+            filename,
+            styleValue
+          )
+        );
+      }
+    } else {
+      for (let i = 0; i < filesArray.length; i++) {
+        const filename = path.parse(filesArray[i]).name;
+        writeFileSync(
+          join(__dirname, `../til/${filename}.html`),
+          writeFile.htmlCreator(
+            readFile.syncReadFile(join("../../", inputValue, filesArray[i])),
+            filename
+          )
+        );
+      }
     }
   }
 }
--- src/utility/writeFile.ts
+++ src/utility/writeFile.ts
@@ -1,4 +1,8 @@
-module.exports.htmlCreator = function (line: string[], title: string) {
+module.exports.htmlCreator = function (
+  line: string[],
+  title: string,
+  stylesheetURL: string = ""
+) {
   var newLine = "";
   for (let i = 0; i < line.length; i++) {
     if (line[i] === "") continue;
@@ -10,6 +14,11 @@ module.exports.htmlCreator = function (line: string[], title: string) {
     <meta charset="utf-8">
     <title>${title}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    ${
+      stylesheetURL === ""
+        ? ""
+        : `<link rel="stylesheet" href="${stylesheetURL}">`
+    }
   </head>
   <body>
     <h1>${title}</h1>
