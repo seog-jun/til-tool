cd70a6577dce09c6f8fcd474e9d00563c0efbda8
seog-jun <shong69@myseneca.ca>
2023-11-09 22:22:47 -0500

    Add unit tests readDirectory, readFile

---
+++ tests/unit/readFile.test.ts
@@ -0,0 +1,23 @@
+import { readFile } from '../../src/utility/readFile'
+
+describe('readFile() testing', () => {
+    test('readFile() with proper param', () => {
+        const res = readFile('../../example1.txt')
+        expect(res).toEqual([
+            'This is the first paragraph.',
+            'This is the second paragraph.',
+        ])
+    })
+
+    test('readFile() with invalid param ', () => {
+        const mockConsole = jest
+            .spyOn(console, 'log')
+            .mockImplementation(() => {})
+        const mockExit = jest.spyOn(process, 'exit').mockImplementation()
+        readFile('')
+        expect(mockConsole).toHaveBeenCalledWith('Invalid input')
+        expect(mockExit).toHaveBeenCalledWith(-1)
+        mockConsole.mockRestore()
+        mockExit.mockRestore()
+    })
+})
